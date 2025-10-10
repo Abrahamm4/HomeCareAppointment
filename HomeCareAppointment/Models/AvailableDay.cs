fb229f317662ff881bb0c394d0db623e36bdbb31
Abrahamm4 <144106356+Abrahamm4@users.noreply.github.com>
2025-10-10 13:03:08 +0200

    Created AvailableDay model/controller
    Model  and Controller for AvailableDays

---
+++ HomeCareAppointment/Models/AvailableDay.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeCareAppointment.Models
+{
+    public class AvailableDay
+    {
+        public int Id { get; set; }
+        public string PersonnelName { get; set; } = "";
+        public DateTime Date { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public bool IsBooked { get; set; }
+
+        //  public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
