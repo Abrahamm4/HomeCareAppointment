3d9600aacbecd2480acd63a14bd847b773e95a20
Abrahamm4 <144106356+Abrahamm4@users.noreply.github.com>
2025-10-11 19:43:51 +0200

    Add Personell model and connect to AvailableDays
    Add Personell model and connect to AvailableDays
    
    - Expanded database schema to include Personell
    - Linked Personell to AvailableDays for scheduling
    - Prepared structure for lazy loading implementation

---
+++ HomeCareAppointment/Models/Personnel.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeCareAppointment.Models
+{
+    public class Personnel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        // Navigation property (one-to-many)
+        public ICollection<AvailableDay>? AvailableDays { get; set; }
+    }
+}
