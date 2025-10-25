71724567f2e7b2f6c52e2813a0503292b139476b
Thars2608 <143402345+Thars2608@users.noreply.github.com>
2025-10-25 19:06:41 +0200

    Small change that didnt push
    Didnt get included in the last push

--- HomeCareAppointment/DAL/IPatientRepository.cs
+++ HomeCareAppointment/DAL/IPatientRepository.cs
@@ -4,8 +4,11 @@ namespace HomeCareAppointment.DAL
 {
     public interface IPatientRepository
     {
+        //Basis CRUD
         Task<IEnumerable<Patient>?> GetAllAsync();
         Task<Patient?> GetByIdAsync(int id);
+        
+        //Full CRUD
         Task<bool> CreateAsync(Patient patient);
         Task<bool> UpdateAsync(Patient patient);
         Task<bool> DeleteAsync(int id);
